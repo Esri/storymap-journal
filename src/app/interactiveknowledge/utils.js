46f9a0da55ccff4ffed0e4eee051f2d827a683d5
Philip Curley <philip@interactiveknowledge.com>
2019-05-07 16:17:39 -0400

    Update section navigation

--- src/app/interactiveknowledge/utils.js
+++ src/app/interactiveknowledge/utils.js
@@ -2,18 +2,43 @@ define([],
 	function()
 	{
     return {
+      /**
+       * Alter the sections per requirements of IK design for LEAF project.
+       * 
+       * @param {array} sections 
+       */
       alterSections: function (sections) {
         sections.forEach(function (section, index, thisSection) {
-          if (index + 1 !== thisSection.length) {
-            var actionName = 'IK-SECTION-' + index.toString();
-            section.content = section.content + '<div class="next-section"><a data-storymaps="' + actionName + '" data-storymaps-type="navigate" href="#"><span>NEXT SECTION</span></div>';
+          var firstSection = false;
+          var lastSection = false;
+
+          if (index === 0) {
+            firstSection = true;
+          }
+
+          if (index + 1 === thisSection.length) {
+            lastSection = true;
+          }
+
+          if (firstSection === false) {
+            var actionName = 'IK-SECTION-BACK' + index.toString();
+            section.content = '<div class="previous-section"><a data-storymaps="' + actionName + '" data-storymaps-type="navigate" href="#"><span>BACK</span></div>' + section.content;
+            section.contentActions.push({
+              id: actionName,
+              type: 'navigate',
+              index: index - 1
+            });
+          }
+
+          if (lastSection === false) {
+            var actionName = 'IK-SECTION-NEXT' + index.toString();
+            section.content = section.content + '<div class="next-section"><a data-storymaps="' + actionName + '" data-storymaps-type="navigate" href="#">' + thisSection[index + 1].title + '</div>';
             section.contentActions.push({
               id: actionName,
               type: 'navigate',
               index: index + 1
             });
           }
-
         });
 
         return sections;
